a32d244404c475865084dfb0b8b2358d014c5ad4
妍 吴 <13421008556@139.com>
2021-05-31 15:23:42 +0800

    Initial Commit

---
+++ pages/demo/demo.js
@@ -0,0 +1,89 @@
+// pages/demo/demo.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    console.log(options)
+    wx.getUserInfo({
+      success: this.setUserInfo.bind(this)
+    })
+    this.setData({
+
+    })
+
+  },
+
+  setUserInfo: function(res){
+    this.setData({user: res.userInfo})
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    wx.hideTabBar({
+      animation: true,
+    })
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  goto_index: function(res){
+    wx.switchTab({
+      url: '../test/test',
+    })
+  }
+
+  
+})
---
+++ pages/test/test.js
@@ -0,0 +1,89 @@
+// pages/test/test.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    num:"",
+    phonenum:"",
+    scanCodeMsg:""
+  },
+  scanCode: function() {
+    var that = this;
+    wx.scanCode({ //扫描API
+      success(res) { //扫描成功
+        console.log(res) //输出回调信息
+        that.setData({
+          scanCodeMsg: res.result
+        });
+        wx.showToast({
+          title: '成功',
+          duration: 1000
+        })
+      }
+    })
+  },
+
+  buttonListen: function(){
+    var that = this;
+    wx.switchTab({
+      url: '../list/list?numData='+that.data.num+'&phonenumData='+that.data.phonenum+'&msgData='+that.data.scanCodeMsg+'',
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
