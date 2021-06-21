c69f2cef53cf3c4f12f95a770cacbdcd5c387ebd
CaptainLightningMeteor <1424673370@qq.com>
2021-06-22 00:35:34 +0800

    完成应用简单工厂模式的初步注册功能

--- miniprogram/pages/register/register.js
+++ miniprogram/pages/register/register.js
@@ -99,50 +99,33 @@ Page({
     user_kind=e.detail.value;
     console.log('radio发生change事件，携带value值为：', user_kind)
   },
+  registerFactory:function(role,userid) {//简单工厂函数
+    console.log("factory start");
+    function user(opt) {
+      this.name=opt.name;
+      this.password=opt.password;
+      this.kind=opt.kind; 
+      this.id=userid;
+      console.log("producing user");
+    }
+    switch(role){
+      case "USER":
+        return new user({name:register_name,password:register_password,kind:"用户"});
+        break;
+      case "BRANCH_DEPOSITORY_MANAGER":
+        return new user({name:register_name,password:register_password,kind:"分仓管理员"});
+        break;
+      case "MAIN_DEPOSITORY_MANAGER":
+        return new user({name:register_name,password:register_password,kind:"总仓管理员"});
+        break;
+    }
+  },
   gotoRegisterNewuser:function(res){
     user_register.get({
       success:(res)=>{
         let user_register_info = res.data;
-        console.log(register_name+' '+register_password+' '+confirm_password+' '+user_kind);
-      
-      if(register_name) {        
-      }
-      else
-      {
-          wx.showToast({
-            title: '请填写完整的注册信息！',
-            icon: 'none',
-            duration: 2000
-         }) 
-      }
-      if(register_password) {        
-      }
-      else
-      {
-          wx.showToast({
-            title: '请填写完整的注册信息！',
-            icon: 'none',
-            duration: 2000
-         }) 
-      }
-        if(confirm_password) {        
-      }
-      else
-      {
-          wx.showToast({
-            title: '请填写完整的注册信息！',
-            icon: 'none',
-            duration: 2000
-         }) 
-      }
-        /*
-       if(register_name==null||register_password==null||confirm_password==null){
-        wx.showToast({
-          title: '请填写完整的注册信息！',
-          icon: 'none',
-          duration: 500
-        }) 
-      }*/
+        //console.log(register_name+' '+register_password+' '+confirm_password+' '+user_kind);
+       if(register_name&&register_password&&confirm_password){
         for (let i = 0; i < user_register_info.length; i++) {  //遍历数据库对象集合
           if (register_name === user_register_info[i].user_name) { //用户名存在
             wx.showToast({
@@ -151,6 +134,7 @@ Page({
               duration: 500
             })
           }
+        }
           if(register_password!=confirm_password){
             wx.showToast({
               title: '确认密码错误！！',
@@ -158,10 +142,38 @@ Page({
               duration: 500
             })
           }
-        }
-       
+          else if(register_password.length<8||register_password.length>24){
+            wx.showToast({
+              title: '密码长度必须为8~24位！！',
+              icon: 'none',
+              duration: 500
+            })
+          }
+          else{
+            console.log("success "+register_name+' '+register_password+' '+confirm_password+' '+user_kind);
+            let new_user=this.registerFactory(user_kind,user_register_info.length+1);
+            console.log("factory has produced a new accout:"+new_user.name+' '+new_user.password+' '+new_user.kind+' '+new_user.id);
+            user_register.add({
+              data:{
+                password:new_user.password,
+                user_id:new_user.id,
+                user_name:new_user.name,
+                userid:new_user.kind
+              },
+              success(res){
+                console.log('注册成功');
+              }
+            })
+          }      
+      }
+      else{
+        wx.showToast({
+          title: '请填写完整的注册信息！',
+          icon: 'none',
+          duration: 500
+        }) 
+      }
       }
     })
-
   }
 })
